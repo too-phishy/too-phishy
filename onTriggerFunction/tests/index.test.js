71b03493e574258e98a79aaad1bb28381ebd8354
Lydia Stepanek <lydia.stepanek@gmail.com>
2025-09-03 13:22:22 -0400

    fix tests

--- onTriggerFunction/tests/index.test.js
+++ onTriggerFunction/tests/index.test.js
@@ -1,19 +1,43 @@
 import { processMessage } from "../src/processMessage.js";
 import { message_body_with_two_bitly_links } from "./fixtures/message_body_with_two_bitly_links.js";
 import { sectionsForCodeHostingSiteLink } from "../src/sections/sectionsForCodeHostingSiteLink.js";
-import { BITLY_PHISHING_SITE_DOMAIN } from "../src/cards/cardForActiveUser.js";
+import {
+  BITLY_PHISHING_SITE_DOMAIN,
+  GCP_PHISHING_SITE_DOMAIN,
+} from "../src/cards/cardForActiveUser.js";
 
 describe("sectionForGCP", () => {
-  test("no gcp link", async () => {
+  test("bitly link is present", async () => {
     try {
       const { headers, fullLinkURIs, messageBodies, attachments } =
         await processMessage(message_body_with_two_bitly_links);
-      const { phishingLinkFlagged: sectionForGCPFlagged, section: gcpSection } =
-        sectionsForCodeHostingSiteLink(
-          fullLinkURIs,
-          BITLY_PHISHING_SITE_DOMAIN,
-          "https://www.bleepingcomputer.com/news/security/phishing-attack-uses-azure-blob-storage-to-impersonate-microsoft"
-        );
+      const {
+        codeHostingSiteFlagged: sectionForBitlyFlagged,
+        sections: bitlySections,
+      } = sectionsForCodeHostingSiteLink(
+        fullLinkURIs,
+        BITLY_PHISHING_SITE_DOMAIN,
+        "https://www.bleepingcomputer.com/news/security/phishing-attack-uses-bitly-blob-storage-to-impersonate-microsoft"
+      );
+
+      expect(sectionForBitlyFlagged).toBe(true);
+    } catch (e) {
+      console.log(e);
+    }
+  }, 20000);
+
+  test("no gcp link is present", async () => {
+    try {
+      const { headers, fullLinkURIs, messageBodies, attachments } =
+        await processMessage(message_body_with_two_bitly_links);
+      const {
+        codeHostingSiteFlagged: sectionForGCPFlagged,
+        sections: gcpSections,
+      } = sectionsForCodeHostingSiteLink(
+        fullLinkURIs,
+        GCP_PHISHING_SITE_DOMAIN,
+        "https://www.bleepingcomputer.com/news/security/phishing-campaign-uses-google-cloud-services-to-steal-office-365-logins/"
+      );
 
       expect(sectionForGCPFlagged).toBe(false);
     } catch (e) {
