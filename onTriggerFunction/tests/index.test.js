f61ee78679945a84b94dc9f7388f507f9cbea441
Lydia Stepanek <lydia.stepanek@gmail.com>
2025-11-26 11:29:08 -0500

    add back tests for fixtures

--- onTriggerFunction/tests/index.test.js
+++ onTriggerFunction/tests/index.test.js
@@ -6,8 +6,46 @@ import {
   GCP_PHISHING_SITE_DOMAIN,
 } from "../src/cards/cardForActiveUser.js";
 import { email_with_parsing_error } from "./fixtures/email_with_parsing_error.js";
+import { email_with_multiple_attachments } from "./fixtures/email_with_multiple_attachments.js";
+import { another_email_with_multiple_attachments } from "./fixtures/another_email_with_multiple_attachments.js";
+import { email_with_link_that_doesnt_get_categorized_correctly } from "./fixtures/email_with_link_that_doesnt_get_categorized_correctly.js";
 
 describe("processMessage", () => {
+  test("process email with multiple attachments", async () => {
+    try {
+      const { headers, fullLinkURIs, messageBodies, attachments } =
+        await processMessage(email_with_multiple_attachments);
+
+      expect(fullLinkURIs.length).toBe(12);
+    } catch (e) {
+      console.log(e);
+    }
+  }, 20000);
+
+  test("process another email with multiple attachments", async () => {
+    try {
+      const { headers, fullLinkURIs, messageBodies, attachments } =
+        await processMessage(another_email_with_multiple_attachments);
+
+      expect(fullLinkURIs.length).toBe(3);
+    } catch (e) {
+      console.log(e);
+    }
+  }, 20000);
+
+  test("process email with link that doesn't get categorized correctly", async () => {
+    try {
+      const { headers, fullLinkURIs, messageBodies, attachments } =
+        await processMessage(
+          email_with_link_that_doesnt_get_categorized_correctly
+        );
+
+      expect(fullLinkURIs.length).toBe(75);
+    } catch (e) {
+      console.log(e);
+    }
+  }, 20000);
+
   test("process emails containing attachments", async () => {
     try {
       const { headers, fullLinkURIs, messageBodies, attachments } =
@@ -20,7 +58,7 @@ describe("processMessage", () => {
   }, 20000);
 });
 
-describe("sectionForGCP", () => {
+describe("sectionsForCodeHostingSiteLink", () => {
   test("bitly link is present", async () => {
     try {
       const { headers, fullLinkURIs, messageBodies, attachments } =
